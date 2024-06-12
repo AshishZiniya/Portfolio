3d53c52aba58f986998ea2afbfbf541700625f32
Ziniya Ashish <98575396+AshishZiniya@users.noreply.github.com>
2024-06-12 11:39:30 +0530

    Update Socials.jsx

--- components/Socials.jsx
+++ components/Socials.jsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 import { FaGithub, FaLinkedin, FaYoutube, FaTwitter } from "react-icons/fa";
 
 const socials = [
-    { icon: <FaGithub />, path: "" },
-    { icon: <FaLinkedin />, path: "" },
+    { icon: <FaGithub />, path: "https://github.com/AshishZiniya" },
+    { icon: <FaLinkedin />, path: "https://www.linkedin.com/in/ashish-ziniya-11a538216/" },
     { icon: <FaYoutube />, path: "" },
     { icon: <FaTwitter />, path: "" },
 ];
